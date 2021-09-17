183083a742e92ff3682f9722f410f37e2786aec5
Riyaz <riyazpasha880@gmail.com>
2021-09-17 11:04:07 +0530

    questions. initial commit

---
+++ question1.py
@@ -0,0 +1,6 @@
+n=int(input())
+arr = []
+
+for i in range(n):
+    ele = int(input())
+    arr.append(ele)
---
+++ question2.py
@@ -0,0 +1,7 @@
+n=int(input())
+
+arr = []
+
+for i in range(n):
+    ele = int(input())
+    arr.append(ele)
