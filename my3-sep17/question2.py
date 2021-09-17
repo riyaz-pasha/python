5651473fa9ad94bbcf7ae3ef6c3703aede6d3d37
Riyaz <riyazpasha880@gmail.com>
2021-09-17 11:56:57 +0530

    bill

--- my3-sep17/question2.py
+++ my3-sep17/question2.py
@@ -1,7 +1,16 @@
 n=int(input())
 
-arr = []
-
-for i in range(n):
-    ele = int(input())
-    arr.append(ele)
+totalbill=0
+if(n>300):
+    totalbill+=(n-300)*15
+    n=300
+if(n>200):
+    totalbill+=(n-200)*10
+    n=200
+if(n>100):
+    totalbill+=(n-100)*7
+    n=100
+if(n>=1):
+    totalbill+=n*5
+    n=0
+print(totalbill)
