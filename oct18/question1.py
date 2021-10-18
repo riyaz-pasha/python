2142f33eede79437060bfdc2f71be29d42e8b57b
Riyaz <riyazpasha880@gmail.com>
2021-10-18 17:30:51 +0530

    sort alternavely

---
+++ oct18/question1.py
@@ -0,0 +1,7 @@
+nums =list(set(map(int, list(input()))))
+size=len(nums)
+for i in range(0,size//2):
+        print(nums[size-i-1])
+        print(nums[i])
+if(size/2!=size//2):
+    print(nums[size//2])
